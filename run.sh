ee7874495b8e812876d518e63831af196cdaaf90
Tom <tomwhitley@gmail.com>
2025-04-27 13:24:37 -0400

    Editing script to see log

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Launch PHP built-in server in the background
-php -S localhost:6500 > /dev/null 2>&1 &
+php -S localhost:6500
 
 # Optional: Wait briefly to ensure server starts before opening browser
 sleep 1
